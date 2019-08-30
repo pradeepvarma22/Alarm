2747500a616afa4a8b51562ea4bc7c4b26eaaf52
varma7 <pradeepvarma107@gmail.com>
2019-08-30 00:37:38 -0700

    Revert "Add : Basic Gui"
    This reverts commit cff474cef69c2bd337f9515c8e3437497ce912a4.

--- alarm.py
+++ alarm.py
@@ -1,29 +1,8 @@
 import datetime as dt
 import os
-from tkinter import *
-def gui_screen():
-    root = Tk()
-    root.title("Alarm")
-    root.geometry("500x400")
-    heading = Label(root,text="WakeUp",font=("arial",40,"bold"),fg="steelblue").pack()
-    label1 = Label(root,text="Hrs",font=("arial",15,"bold")).place(x=8,y=150)
-    nhrs = int()
-    entry_box=Entry(root,textvariable=hrs,width=5).place(x=8,y=180)
 
-    label2 = Label(root, text="Min", font=("arial", 15, "bold")).place(x=9, y=220)
-    nmin = int()
-    entry_box = Entry(root, textvariable=hrs, width=5).place(x=10, y=250)
-
-    
-
-
-
-    root.mainloop()
-
-
-gui_screen()
-#hrs = int(input("Hrs- "))
-#min = int(input("Min- "))
+hrs = int(input("Hrs- "))
+min = int(input("Min- "))
 
 count = 0
 while(1):
@@ -37,6 +16,7 @@ while(1):
 if count==1:
     os.system("sample.mp3")
     exit(0)
+
 
 
 
