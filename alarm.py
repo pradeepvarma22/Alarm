9440aeb4019a19fabf28791cdbf476d43525f134
varma7 <pradeepvarma107@gmail.com>
2019-10-14 08:51:58 +0530

    Add : Am/Pm and Min Changes
    Signed-off-by: varma7 <pradeepvarma107@gmail.com>

--- alarm.py
+++ alarm.py
@@ -3,24 +3,22 @@ import os
 
 hrs = int(input("Hrs- "))
 min = int(input("Min- "))
+getampm = input("AM/PM- ")
+getampm = getampm.upper()
 
 count = 0
 while(1):
     nhr = dt.datetime.now().hour
     nmin = dt.datetime.now().minute
-    if (nhr > 12):
-        nhr = nhr - 12
-    if hrs == nhr and min == nmin:
-        count=1
-        break
+    temp = dt.datetime.now()
+    store2 = temp.strftime("%p")
+    osampm = store2[-2:]
+    if getampm == osampm:
+        if (nhr > 12):
+            nhr = nhr - 12
+        if hrs == nhr and min == nmin:
+            count=1
+            break
 if count==1:
     os.system("sample.mp3")
     exit(0)
-
-
-
-
-
-
-
-
